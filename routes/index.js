2e0f93071600c6c2bebe924fa73823904002405c
Parker Mullenax <117956149+pmull011@users.noreply.github.com>
2026-03-11 12:26:16 -0400

    added pages for recalls

--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,40 @@ const connection = mysql.createConnection({
 });
 
 router.get("/", (req, res) =>{
-    connection.query(`SELECT 
-                        recall.recall_id,
-                        recall.description,
-                        DATE_FORMAT(recall.date, '%Y-%m-%d') AS recall_date,
-                        recall.company,
-                        recall.regions,
-                        recall.amount_sick,
-                        recall.amount_dead,
-                        recall.classification,
-                        product.product_name
-                        FROM recall
-                        JOIN product
-                        ON product.product_id = recall.product_id
-                        ORDER BY recall.date DESC`,
-                        function (error, results, fields) {
-        if (error != null) { console.log(error);
-            return res.status(500).send("Database error");
-        }
-    return res.render("index", {title: "Home", recall_data: results});
+    const perPage = 10;
+    const page = parseInt(req.query.page) || 1;
+    const offset = (page - 1) * perPage;
+
+    connection.query("SELECT COUNT(*) AS total FROM recall", function (error, countResult) {
+
+    const totalRecalls = countResult[0].total;
+    const totalPages = Math.ceil(totalRecalls / perPage);
+
+        connection.query(`SELECT 
+                            recall.recall_id,
+                            recall.description,
+                            DATE_FORMAT(recall.date, '%Y-%m-%d') AS recall_date,
+                            recall.company,
+                            recall.regions,
+                            recall.amount_sick,
+                            recall.amount_dead,
+                            recall.classification,
+                            product.product_name
+                            FROM recall
+                            JOIN product
+                            ON product.product_id = recall.product_id
+                            ORDER BY recall.date DESC
+                            LIMIT ${perPage} OFFSET ${offset}`,
+                            function (error, results, fields) {
+                            if (error != null) { console.log(error);
+                                return res.status(500).send("Database error");
+                            }
+        return res.render("index", {title: "Home",
+                                    recall_data: results,
+                                    currentPage: page,
+                                    totalPages: totalPages
+            });
+        });
     });
 });
 
